7918e8d1e7fbed532daace11232c53f0396fce34
yagmurrizvi <50454736+yagmurrizvi@users.noreply.github.com>
2022-04-15 09:59:15 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import dictionary from "./dictionary.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <h1>Dictionary Project</h1>
+      <img src={dictionary} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
